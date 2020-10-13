2a2a30fc351a481c47de17a80b2936bdfd9f2be4
lshi <Lisa.Shi@clearcom.com>
2020-10-12 21:49:16 -0400

    explicate specializtion for string

--- modenCPP/max/prettyPrint.cpp
+++ modenCPP/max/prettyPrint.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 // template class
-
 		
-		
-		template<typename T>
+template<typename T>
 class  PrettyPrint 
 {
 		T *m_pData; // decoration here is pointer to pointer
@@ -21,6 +19,27 @@ public:
 
 };
 
+// Explicite specialization for string types:q
+template<>
+
+class  PrettyPrint <char*>
+{
+		char *m_pData;
+public:
+		PrettyPrint (char *data) :m_pData(data) { // becames T**  so augment passed should be pointer to pointer
+		}
+		void Print() {
+		    std::cout << "{"<< 
+					m_pData <<"}" <<std::endl;
+		}
+
+		char * GetData() {
+				return m_pData;
+		}
+
+};
+
+
 int main()
 {
 		int data = 5;
@@ -32,8 +51,9 @@ int main()
 		char *p { "hello world" };
 			// when use char* as template parameter then the type of T is T* , 	
 //		PrettyPrint <char*> p3(p);  // ERROR "cant covert fron char* to char**
-		PrettyPrint <char*> p3(&p);
+		PrettyPrint <char*> p3(p);
 		p3.Print();
+	   char *pData = p3.GetData();
 		return 0;
 
 }
